084d0d690721ac69ee0339f2bcd677aa9af290af
Sayantan Mishra <sayanmdn@gmail.com>
2020-07-25 18:44:45 +0530

    ReduxDemo working

--- src/reduxDemo.js
+++ src/reduxDemo.js
@@ -3,12 +3,21 @@ import { createStore } from 'redux'
 
 class ReduxDemo extends Component {
     render() {
-        // Create Store (reducer, state)
-        //const store = createStore(reducer, "Peace")
-
         //set Reducer (state, action)
+        const reducer = function (state, action) {
+            if (action.type === "ATTACK") {
+                return action.task
+            }
+            return state
+        }
 
+        // Create Store (reducer, state)
+        const store = createStore(reducer, "Peace")
 
+        store.subscribe(() => {
+            console.log("Now store is ", store.getState())
+        })
+        store.dispatch({ type: "ATTACK", task: "Sayantan" })
 
         return (
             <div>
