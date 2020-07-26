df406950ad689d964552b322905d98e2b4f6ce6a
Sayantan Mishra <sayanmdn@gmail.com>
2020-07-26 22:54:40 +0530

    redux functions out of class

--- src/ReduxDemo2.js
+++ src/ReduxDemo2.js
@@ -1,34 +1,41 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 
+//set Reducer (state, action)
+const reducer = function (state, action) {
+    if (action.type === "changeState") {
+        return action.payload.newState
+    }
+    return state
+}
+
+// Create Store (reducer, state)
+const store = createStore(reducer, "oldState")
+
+//to print the state of the store
+console.log(store.getState())
+
+// store.subscribe(() => {
+//     console.log("Now state of store is ", store.getState())
+// })
+store.dispatch({ type: "changeState", payload: { newState: "updated to new state" } })
+// console.log(store.getState())
+// handleChange(){
+//     store.dispatch({type:"addOne", payload:{value: store.getState()}})
+// }
 class ReduxDemo2 extends Component {
     render() {
-        //set Reducer (state, action)
-        const reducer = function (state, action) {
-            if (action.type === "changeState") {
-                return action.payload.newState
-            }
-            return state
-        }
-
-        // Create Store (reducer, state)
-        const store = createStore(reducer, "oldState")
-
-        //to print the state of the store
-        console.log(store.getState())
-
-        // store.subscribe(() => {
-        //     console.log("Now state of store is ", store.getState())
-        // })
-        store.dispatch({ type: "changeState", payload: {newState: "This is payload/newState"} })
-        console.log(store.getState())
-
         return (
             <div>
                 Test ReduxDemo2
+                <p>Now state is {store.getState()}</p>
             </div>
         )
     }
+}
+
+class reduxDemoCombine extends Component {
+
 }
 
 export default ReduxDemo2
