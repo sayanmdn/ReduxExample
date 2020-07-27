2ad933f1d1a8f03912a1ed90a814f18d81088258
Sayantan Mishra <sayanmdn@gmail.com>
2020-07-27 23:52:24 +0530

    user reducer update

--- src/reduxDemoCombine.js
+++ src/reduxDemoCombine.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react'
-// import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
+
+function ProductsReducer(state=[], action) {
+    return state
+}
+function UserReducer (state='',action){
+    switch (action.type){
+        case 'newUser':
+            return action.payload
+        default : return state
+    }
+    // return state
+}
+const allReducers = combineReducers({
+    products: ProductsReducer,
+    user : UserReducer
+})
+const store = createStore (allReducers,{
+    products:[{name:'iPhone'}],
+    user:'TestUser'
+})
+console.log(store.getState())
+const updateUserAction = {
+    type:'newUser',
+    payload : {
+        user : 'Sayantan'
+    }
+}
+store.dispatch(updateUserAction)
+console.log(store.getState())
+
 
 class ReduxDemoCombine extends Component {
     render() {
