07c8f7b623ea27c1071ebae2cc1fbc717c9490f5
Sayantan Mishra <sayanmdn@gmail.com>
2020-08-11 05:10:06 +0530

    Signin add

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ReduxDemoCombine from './reduxDemoCombine.js'
 // import { updateUser } from './actions/user-action'
 import Contact from './contact'
 import { Route, Link } from 'react-router-dom'
+import SignIn from './signin'
 class App extends Component {
 
   // constructor (props){
@@ -25,17 +26,23 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          <a href='http://localhost:3000'>
-            <code>Link to Home</code>
-          </a>
+          <h2>Welcome to React</h2>
+          <div>
+            <p>
+              <a href='http://localhost:3000'>
+                Link to Home
+            </a>
+            &tab
+            <a href='http://localhost:3000/signin'>
+                Link to Signin
+            </a>
+            </p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Route exact path='/contact' component={Contact} />
         <div>
-        <Route exact path='/counter' component={CounterApp} />
+          <Route exact path='/counter' component={CounterApp} />
+          <Route exact path='/signin' component={SignIn} />
         </div>
         <br></br>
         <div>
